fe2263cd93938794bc56a0827eb4cf519d72b7d2
AtillaA <atillanalcaci@sabanciuniv.edu>
2026-09-19 15:00:22 +0300

    Update deterministic.py

--- PrimeCalculator/deterministic.py
+++ PrimeCalculator/deterministic.py
@@ -1,15 +1,5 @@
 import math
 
-"""
-Deterministic algorithms for detecting whether a given positive integer is prime
-
-These algorithms prove definitively whether a number is prime or composite, there are no false positives or false negatives
-They can be computationally heavy for massive numbers
-
-Three major tests are: Trial Division Test, AKS Test, Lukas-Lehmer Test
-"""
-
-# -------------------------------------------
 
 """
 Trial Division Test:
