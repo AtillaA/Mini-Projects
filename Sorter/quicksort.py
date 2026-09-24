8ebfc0bae2473bc7d66f21545c2050c470bb2a33
Atilla Nalcaci <atillanalcaci@sabanciuniv.edu>
2026-09-24 22:05:54 +0200

    Create quicksort.py

---
+++ Sorter/quicksort.py
@@ -0,0 +1,22 @@
+def quicksort(V, l, r):
+    if l >= r:
+        return
+    i = l
+    j = r
+    p = (l+r)//2 # pivot in the middle
+    x = V[p]
+    while i<j:
+        while x > V[i]:
+            i+=1
+        while V[j] > x:
+            j-=1
+        if i <= j:
+            swap(V, i, j) # auxiliary procedure
+            i+=1
+            j-=1
+    if l<j:
+        quicksort(V, l, j)
+    if i<r:
+        quicksort(V, i, r)
+    return V
+  
