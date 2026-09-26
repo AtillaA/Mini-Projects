d6f1f72282e9d3e9078efcd35e1539af3218713c
AtillaA <atillanalcaci@sabanciuniv.edu>
2026-09-26 14:19:10 +0200

    Update test_handler.py to accomodate integer/list distinction

--- PrimeCalculator/test_handler.py
+++ PrimeCalculator/test_handler.py
@@ -4,31 +4,64 @@ from probabilistic import fermat_test, miller_rabin_test, baillie_psw_test
 
 
 
-# INCOMPLETE: ADD FUNCTIONALITY FOR DETERMINING THE OUTPUT BASED ON THE OPTION
+class Sieve:
+    """ natively lists primes up to n, derives the single-number check """
+    def __init__(self, func):
+        self.func = func
+
+    def is_prime(self, n):
+        return n in self.func(n)
+
+    def primes_up_to(self, n):
+        return self.func(n)
+
+
+class PrimalityTest:
+    """ natively checks one number, derives the list """
+    def __init__(self, func):
+        self.func = func
+
+    def is_prime(self, n):
+        return self.func(n)
+
+    def primes_up_to(self, n):
+        return [k for k in range(2, n + 1) if self.func(k)]
+
+
 class TestHandlers:
+    def run(self, method, option, n):
+        """ the only place that checks the option """
+        if n < 2:  # nothing below 2 is prime, regardless of the method
+            result = False if option == 1 else []
+        elif option == 1:
+            result = method.is_prime(n)
+        else:
+            result = method.primes_up_to(n)
+        print(result)
+
     def handle_1(self, option, n): # ERATOSTHENES
-        sieve_of_eratosthenes(n)
+        self.run(Sieve(sieve_of_eratosthenes), option, n)
 
     def handle_2(self, option, n): # SUNDARAM
-        sieve_of_sundaram(n)
+        print(sieve_of_sundaram(n))
 
     def handle_3(self, option, n): # ATKIN
-        sieve_of_atkin(n)
+        print(sieve_of_atkin(n))
 
     def handle_4(self, option, n): # TRIAL DIVISION
-        print(trial_division_test(n))
+        self.run(PrimalityTest(trial_division_test), option, n)
 
     def handle_5(self, option, n): # AKS
-        aks_test(n)
+        print(aks_test(n))
 
     def handle_6(self, option, n): #LUCAS-LEHMER
-        lucas_lehmer_test(n)
+        print(lucas_lehmer_test(n))
 
     def handle_7(self, option, n): # FERMAT
-        fermat_test(n)
+        print(fermat_test(n))
 
     def handle_8(self, option, n): # MILLER-RABIN
-        miller_rabin_test(n)
+        print(miller_rabin_test(n))
 
     def handle_9(self, option, n): #BAILLIE-PSW
-        baillie_psw_test(n)
+        print(baillie_psw_test(n))
